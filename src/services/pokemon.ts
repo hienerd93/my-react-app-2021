bdee528f35e02f3fe8c4deda3b718a13c76166cf
shiinez <truong.quang.hien.93@gmail.com>
2022-01-05 10:03:21 +0700

    apply grapql code

--- src/services/pokemon.ts
+++ src/services/pokemon.ts
@@ -1,5 +1,22 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Pokemon } from "data";
+// TODO: graphql extends
+// import { request, gql, ClientError } from "graphql-request";
+
+// TODO: graphql extends
+// const graphqlBaseQuery =
+//   ({ baseUrl }: { baseUrl: string }) =>
+//   async ({ body }: { body: string }) => {
+//     try {
+//       const result = await request(baseUrl, body);
+//       return { data: result };
+//     } catch (error) {
+//       if (error instanceof ClientError) {
+//         return { error: { status: error.response.status, data: error } };
+//       }
+//       return { error: { status: 500, data: error } };
+//     }
+//   };
 
 export const pokemonApi = createApi({
   reducerPath: "pokemonApi",
@@ -9,6 +26,34 @@ export const pokemonApi = createApi({
       query: (name) => `pokemon/${name}`,
     }),
   }),
+  // TODO: graphql extends
+  // baseQuery: graphqlBaseQuery({
+  //   baseUrl: "https://graphql-pokeapi.vercel.app/api/graphql",
+  // }),
+  // endpoints: (builder) => ({
+  //   getPokemonByName: builder.query<Pokemon, string>({
+  //     query: (name) => ({
+  //       body: gql`
+  //       query {
+  //         pokemon(name: "${name}") {
+  //           id
+  //           name
+  //           base_experience
+  //           height
+  //           weight
+  //           species {
+  //             name
+  //           }
+  //           sprites {
+  //             front_shiny
+  //           }
+  //         }
+  //       }
+  //       `,
+  //     }),
+  //     transformResponse: (response) => response.pokemon,
+  //   }),
+  // }),
 });
 
 export const { useGetPokemonByNameQuery } = pokemonApi;
